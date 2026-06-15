629bc4adebc49c0826add588aab965c9c6be4cfe
Santhosh S <santhoshsekar97@gmail.com>
2026-06-15 13:19:56 +0000

    soft assertion

--- tests/api_test/test_events.py
+++ tests/api_test/test_events.py
@@ -4,6 +4,7 @@ from api.events import Events
 from utils.json_reader import JSONReader
 from core.auth_manager import AuthManager
 from datetime import datetime, timedelta
+from pytest_check import check
 
 class TestEvemts:
 
@@ -25,10 +26,10 @@ class TestEvemts:
         response = self.events_api.create_new_events(self.event_data)
         assert response.status_code == 201, f"Expected 201 created but received {response.status_code}"
         response_body = response.json()
-        assert response_body["data"]["title"] == self.event_data["title"], "Incorrect title received from the response"
-        assert response_body["data"]["price"] == str(self.event_data["price"]), "Incorrect price received from the response"
-        assert response_body["data"]["totalSeats"] == self.event_data["totalSeats"], "Incorrect totalSeats received from the response"
-        assert response_body["message"] == "Event created successfully", "event is not confirmed"
+        check.equal(response_body["data"]["title"], self.event_data["title"], "Incorrect title received from the response")
+        check.equal(response_body["data"]["price"], str(self.event_data["price"]), "Incorrect price received from the response")
+        check.equal(response_body["data"]["totalSeats"], self.event_data["totalSeats"], "Incorrect totalSeats received from the response")
+        check.equal(response_body["message"], "Event created successfully", "event is not confirmed")
         self.delete_event(response_body["data"]["id"])
 
     def test_get_all_events(self):
@@ -36,10 +37,10 @@ class TestEvemts:
         response = self.events_api.create_new_events(self.event_data)
         assert response.status_code == 201, f"Expected 201 created but received {response.status_code}"
         response_body = response.json()
-        assert response_body["message"] == "Event created successfully", "event is not confirmed"
+        check.equal(response_body["message"], "Event created successfully", "event is not confirmed")
         all_events_response = self.events_api.get_all_events({})
         assert all_events_response.status_code ==  200, f"Expected 200 but received {all_events_response.status_code}"
-        assert type(all_events_response.json()["data"]) is list, "Its not returned all events"
+        check.equal(type(all_events_response.json()["data"]), list, "Its not returned all events")
         self.delete_event(response_body["data"]["id"])
 
     def test_single_event(self):
@@ -47,10 +48,10 @@ class TestEvemts:
         response = self.events_api.create_new_events(self.event_data)
         assert response.status_code == 201, f"Expected 201 created but received {response.status_code}"
         response_body = response.json()
-        assert response_body["message"] == "Event created successfully", "event is not confirmed"
+        check.equal(response_body["message"], "Event created successfully", "event is not confirmed")
         get_single_event = self.events_api.get_single_event(response_body["data"]["id"])
         assert get_single_event.status_code == 200, f"Expected 200 but received {get_single_event.status_code}"
-        assert get_single_event.json()["data"]["id"] == response_body["data"]["id"], "event id is incorrect"
+        check.equal(get_single_event.json()["data"]["id"], response_body["data"]["id"], "event id is incorrect")
         self.delete_event(response_body["data"]["id"])
 
     def test_delete_event(self):
@@ -58,12 +59,12 @@ class TestEvemts:
         response = self.events_api.create_new_events(self.event_data)
         assert response.status_code == 201, f"Expected 201 created but received {response.status_code}"
         response_body = response.json()
-        assert response_body["message"] == "Event created successfully", "event is not confirmed"
+        check.equal(response_body["message"], "Event created successfully", "event is not confirmed")
         self.delete_event(response_body["data"]["id"])
         all_events_response = self.events_api.get_all_events({})
         assert all_events_response.status_code ==  200, f"Expected 200 but received {all_events_response.status_code}"
         for event in all_events_response.json()["data"]:
-            assert event["id"] != response_body["data"]["id"], "event is not cancelled properly"
+            check.not_equal(event["id"], response_body["data"]["id"], "event is not cancelled properly")
 
 
     def test_update_event(self):
@@ -71,17 +72,17 @@ class TestEvemts:
         response = self.events_api.create_new_events(self.event_data)
         assert response.status_code == 201, f"Expected 201 created but received {response.status_code}"
         response_body = response.json()
-        assert response_body["message"] == "Event created successfully", "event is not confirmed"
+        check.equal(response_body["message"], "Event created successfully", "event is not confirmed")
         self.event_data["title"] = "welcome"
         update_response = self.events_api.update_event(response_body["data"]["id"], self.event_data)
         assert update_response.status_code == 200, "Event is not updated"
-        assert update_response.json()["data"]["title"] == "welcome", "Title is not updated"
+        check.equal(update_response.json()["data"]["title"], "welcome", "Title is not updated")
         self.delete_event(response_body["data"]["id"])
 
     def delete_event(self, event_id):
         event_delete_response = self.events_api.delete_event(event_id)
         assert event_delete_response.status_code == 200, f"Expected 200 but received {event_delete_response.status_code}"
-        assert event_delete_response.json()["message"] == "Event deleted successfully", "event is not deleted"
+        check.equal(event_delete_response.json()["message"], "Event deleted successfully", "event is not deleted")
 
     def get_future_date(self, days=1):
         return (datetime.now() + timedelta(days=days)).strftime("%Y-%m-%dT%H:%M:%S")
--- tests/api_test/test_ticket_booking.py
+++ tests/api_test/test_ticket_booking.py
@@ -3,6 +3,7 @@ from utils.logger import Logger
 from api.ticket_booking import TicketBooking
 from utils.json_reader import JSONReader
 from core.auth_manager import AuthManager
+from pytest_check import check
 
 class TestTicketBooking:
 
@@ -23,11 +24,11 @@ class TestTicketBooking:
         response = self.bookings_api.create_new_booking(self.bookings_data)
         assert response.status_code == 201, f"Expected 201 created but received {response.status_code}"
         response_body = response.json()
-        assert response_body["data"]["eventId"] == self.bookings_data["eventId"], "Incorrect eventId received from the response"
-        assert response_body["data"]["customerName"] == self.bookings_data["customerName"], "Incorrect customerName received from the response"
-        assert response_body["data"]["customerEmail"] == self.bookings_data["customerEmail"], "Incorrect customerEmail received from the response"
-        assert response_body["data"]["status"] == "confirmed", "Incorrect status received from the response"
-        assert response_body["message"] == "Booking confirmed!", "Booking is not confirmed"
+        check.equal(response_body["data"]["eventId"], self.bookings_data["eventId"], "Incorrect eventId received from the response")
+        check.equal(response_body["data"]["customerName"], self.bookings_data["customerName"], "Incorrect customerName received from the response")
+        check.equal(response_body["data"]["customerEmail"], self.bookings_data["customerEmail"], "Incorrect customerEmail received from the response")
+        check.equal(response_body["data"]["status"], "confirmed", "Incorrect status received from the response")
+        check.equal(response_body["message"], "Booking confirmed!", "Booking is not confirmed")
         self.cancel_booking(response_body["data"]["id"])
 
     def test_get_all_bookings(self):
@@ -35,10 +36,10 @@ class TestTicketBooking:
         response = self.bookings_api.create_new_booking(self.bookings_data)
         assert response.status_code == 201, f"Expected 201 created but received {response.status_code}"
         response_body = response.json()
-        assert response_body["message"] == "Booking confirmed!", "Booking is not confirmed"
+        check.equal(response_body["message"], "Booking confirmed!", "Booking is not confirmed")
         all_bookings_response = self.bookings_api.get_all_bookings({})
         assert all_bookings_response.status_code ==  200, f"Expected 200 but received {all_bookings_response.status_code}"
-        assert type(all_bookings_response.json()["data"]) is list, "Its not returned all bookings"
+        check.equal(type(all_bookings_response.json()["data"]), list, "Its not returned all bookings")
         self.cancel_booking(response_body["data"]["id"])
 
     def test_single_booking(self):
@@ -46,10 +47,10 @@ class TestTicketBooking:
         response = self.bookings_api.create_new_booking(self.bookings_data)
         assert response.status_code == 201, f"Expected 201 created but received {response.status_code}"
         response_body = response.json()
-        assert response_body["message"] == "Booking confirmed!", "Booking is not confirmed"
+        check.equal(response_body["message"], "Booking confirmed!", "Booking is not confirmed")
         get_single_booking = self.bookings_api.get_single_booking(response_body["data"]["id"])
         assert get_single_booking.status_code == 200, f"Expected 200 but received {get_single_booking.status_code}"
-        assert get_single_booking.json()["data"]["id"] == response_body["data"]["id"], "Booking id is incorrect"
+        check.equal(get_single_booking.json()["data"]["id"], response_body["data"]["id"], "Booking id is incorrect")
         self.cancel_booking(response_body["data"]["id"])
 
     def test_cancel_booking(self):
@@ -57,17 +58,17 @@ class TestTicketBooking:
         response = self.bookings_api.create_new_booking(self.bookings_data)
         assert response.status_code == 201, f"Expected 201 created but received {response.status_code}"
         response_body = response.json()
-        assert response_body["message"] == "Booking confirmed!", "Booking is not confirmed"
+        check.equal(response_body["message"], "Booking confirmed!", "Booking is not confirmed")
         booking_cancel_response = self.bookings_api.cancel_booking(response_body["data"]["id"])
         assert booking_cancel_response.status_code == 200, f"Expected 200 but received {booking_cancel_response.status_code}"
-        assert booking_cancel_response.json()["message"] == "Booking cancelled", "Booking is not cancelled"
+        check.equal(booking_cancel_response.json()["message"], "Booking cancelled", "Booking is not cancelled")
         all_bookings_response = self.bookings_api.get_all_bookings({})
         assert all_bookings_response.status_code ==  200, f"Expected 200 but received {all_bookings_response.status_code}"
         for booking in all_bookings_response.json()["data"]:
-            assert booking["id"] != response_body["data"]["id"], "Booking is not cancelled properly"
+            check.equal(booking["id"] != response_body["data"]["id"], "Booking is not cancelled properly")
 
 
     def cancel_booking(self, booking_id):
         booking_cancel_response = self.bookings_api.cancel_booking(booking_id)
         assert booking_cancel_response.status_code == 200, f"Expected 200 but received {booking_cancel_response.status_code}"
-        assert booking_cancel_response.json()["message"] == "Booking cancelled", "Booking is not cancelled"
+        check.equal(booking_cancel_response.json()["message"], "Booking cancelled", "Booking is not cancelled")
